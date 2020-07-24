52695614eeca38dc985053f76a6ae6578a9464fb
jhell85 <jhell85@gmail.com>
2020-07-23 20:47:18 -0700

    add CommentDetail component as a child to ApprovalCard

--- src/index.js
+++ src/index.js
@@ -2,16 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4 pm"
-        avatar={faker.image.avatar()}
-        text="nice blog post"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4 pm"
+          avatar={faker.image.avatar()}
+          text="nice blog post"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jessica"
+          timeAgo="Today at 6 am"
+          avatar={faker.image.avatar()}
+          text="great content"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Joey"
+          timeAgo="Today at 12 pm"
+          avatar={faker.image.avatar()}
+          text="amazing post"
+        />
+      </ApprovalCard>
     </div>
   );
 };
