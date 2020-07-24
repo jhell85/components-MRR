7e7980b92bcadc3218b0e0958d8ec011f3b7978a
jhell85 <jhell85@gmail.com>
2020-07-23 19:41:15 -0700

    add comment detail as child to index

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
-const App = () => { 
+const App = () => {
   return (
-          <div className="ui container comments"> 
-            <div className="comment">
-              <a href="/" className="avatar">
-                <img alt="avatar" />
-              </a>
-              <div className="content">
-                <a href="/" className="author">
-                  Sam
-                </a>
-                <div className="metadata">
-                  <span className="date">Today</span>
-                </div>
-                <div className="test">Nice blog post</div>
-              </div>
-            </div>
-          </div>
-  )
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4 pm"
+        avatar={faker.image.avatar()}
+        text="nice blog post"
+      />
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"))
+ReactDOM.render(<App />, document.querySelector("#root"));
